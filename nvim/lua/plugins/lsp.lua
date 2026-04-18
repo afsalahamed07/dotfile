b2aeddd62ed5035ab64e2388d269947b71d8ae0f
agrippa <afsalahamed07@gmail.com>
2026-04-18 13:35:58 +0530

    update cssls lint handling

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        cssls = {
+          settings = {
+            css = {
+              validate = true,
+              lint = {
+                unknownAtRules = "ignore",
+              },
+            },
+            scss = {
+              validate = true,
+              lint = {
+                unknownAtRules = "ignore",
+              },
+            },
+            less = {
+              validate = true,
+              lint = {
+                unknownAtRules = "ignore",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
