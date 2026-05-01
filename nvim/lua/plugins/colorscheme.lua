a4cbc448a6332d970404a44c6d3216e4ee8d7e52
agrippa <afsalahamed07@gmail.com>
2026-05-02 00:51:29 +0530

    config update

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,3 +1,13 @@
+local function apply_hubbamax_highlights()
+    local variable = { fg = "#bcbcbc" }
+    vim.api.nvim_set_hl(0, "@variable", variable)
+    vim.api.nvim_set_hl(0, "@variable.go", variable)
+    vim.api.nvim_set_hl(0, "@punctuation.bracket.go", variable)
+    vim.api.nvim_set_hl(0, "@lsp.type.variable", variable)
+    vim.api.nvim_set_hl(0, "@lsp.type.variable.go", variable)
+    vim.api.nvim_set_hl(0, "@lsp.type.punctuation.bracket.go", variable)
+end
+
 return {
     {
         "LazyVim/LazyVim",
@@ -9,5 +19,11 @@ return {
         "alexpasmantier/hubbamax.nvim",
         lazy = false,
         priority = 1000,
+        config = function()
+            vim.api.nvim_create_autocmd("ColorScheme", {
+                pattern = "hubbamax",
+                callback = apply_hubbamax_highlights,
+            })
+        end,
     },
 }
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -3,4 +3,20 @@ return {
         "akinsho/bufferline.nvim",
         enabled = false,
     },
+    {
+        "folke/snacks.nvim",
+        priority = 1000,
+        lazy = false,
+        opts = {
+            picker = {
+                sources = {
+                    explorer = {
+                        layout = {
+                            auto_hide = { "input" },
+                        },
+                    },
+                },
+            },
+        },
+    },
 }
