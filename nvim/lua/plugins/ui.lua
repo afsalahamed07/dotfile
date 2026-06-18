d79cfff91071771fd10b5d78732589ed1a1a59f7
agrippa <afsalahamed07@gmail.com>
2026-06-18 09:40:12 +0530

    more...

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -21,6 +21,18 @@ return {
     },
 
     {
+        "nvim-lualine/lualine.nvim",
+        opts = function(_, opts)
+            opts.options = opts.options or {}
+            opts.options.section_separators = { left = "", right = "" }
+            opts.options.component_separators = { left = "", right = "" }
+            opts.sections = opts.sections or {}
+            opts.sections.lualine_a = vim.list_extend({ { "", padding = { left = 0, right = 0 } } }, opts.sections.lualine_a or {})
+            opts.sections.lualine_z = vim.list_extend(opts.sections.lualine_z or {}, { { "", padding = { left = 0, right = 0 } } })
+        end,
+    },
+
+    {
         "MeanderingProgrammer/render-markdown.nvim",
         enabled = true,
         opts = {
